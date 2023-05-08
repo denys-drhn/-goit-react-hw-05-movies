76a1c5551a191f74f83b6023387e982d9c64cb08
Denys <denys.drhn@gmail.com>
2023-05-08 19:16:36 +0300

    change path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,7 @@ import Layout from './Layout';
 
 const Home = lazy(() => import('../pages/Home'));
 const Movies = lazy(() => import('../pages/Movies'));
-const MovieDetails = lazy(() =>
-  import('../components/MovieDetails/MovieDetails')
-);
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('../components/Cast/Cast'));
 const Reviews = lazy(() => import('../components/Reviews/Reviews'));
 
