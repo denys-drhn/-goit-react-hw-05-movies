e7979fe9266db0b032e8cb2d1be28d85c52d25da
Denys <denys.drhn@gmail.com>
2023-05-08 19:07:31 +0300

    import css & IMAGE_DEFAULT_URL, remove slice

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import fetchMovieCredits from '../../services/fetchMovieCredits';
+import css from './Cast.module.css';
+import IMAGE_DEFAULT_URL from '../../images/default-profile.jpg';
 
 const Cast = () => {
   const { movieId } = useParams();
@@ -18,17 +20,21 @@ const Cast = () => {
   const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/w200/';
 
   return (
-    <div>
-      <ul>
-        {cast.slice(0, 6).map(({ id, name, character, profile_path }) => (
-          <li key={id}>
-            <img src={IMAGES_BASE_URL + profile_path} alt={name} />
-            <p>{name}</p>
-            <p>Character: {character}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className={css.castList}>
+      {cast.map(({ cast_id, name, character, profile_path }) => (
+        <li key={cast_id}>
+          <img
+            className={css.castImg}
+            src={
+              profile_path ? IMAGES_BASE_URL + profile_path : IMAGE_DEFAULT_URL
+            }
+            alt={name}
+          />
+          <p>{name}</p>
+          <p>Character: {character}</p>
+        </li>
+      ))}
+    </ul>
   );
 };
 export default Cast;
