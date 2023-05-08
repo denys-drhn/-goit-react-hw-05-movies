59fd2065cdc5787038d37e9c451a71f8ad95d85d
Denys <denys.drhn@gmail.com>
2023-05-08 23:44:41 +0300

    add ProgressBar in Suspense

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { NavLink, Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { ProgressBar } from 'react-loader-spinner';
 import css from './Layout.module.css';
 
 const Layout = () => {
@@ -12,7 +13,19 @@ const Layout = () => {
         </nav>
       </header>
       <main>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <ProgressBar
+              height="80"
+              width="80"
+              ariaLabel="progress-bar-loading"
+              wrapperStyle={{}}
+              wrapperClass="progress-bar-wrapper"
+              borderColor="#F4442E"
+              barColor="#51E5FF"
+            />
+          }
+        >
           <Outlet />
         </Suspense>
       </main>
