65f025540eaf7f3948433cf2f5f95d13acaad16a
Denys <denys.drhn@gmail.com>
2023-05-08 23:45:56 +0300

    add InfinitySpin

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,16 +1,19 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import fetchMovieReviews from '../../services/fetchMovieReviews';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const Reviews = () => {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getMovieReviews = async () => {
       try {
         const movieReviews = await fetchMovieReviews(movieId);
         setReviews(movieReviews);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -21,7 +24,11 @@ const Reviews = () => {
 
   return (
     <div>
-      {reviews.length > 0 ? (
+      {loading ? (
+        <div>
+          <InfinitySpin width="200" color="#4fa94d" />
+        </div>
+      ) : reviews.length > 0 ? (
         <ul>
           {reviews.map(({ id, author, content }) => (
             <li key={id}>
