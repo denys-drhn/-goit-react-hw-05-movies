d4159fa308e2b9ba92d1e9f229a8a44c0727d71b
Denys <denys.drhn@gmail.com>
2023-05-08 23:45:17 +0300

    add ProgressBar in Suspense & InfinitySpin

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,13 +2,16 @@ import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { Suspense } from 'react';
 import { useState, useEffect } from 'react';
 import fetchMovieDetails from 'services/fetchMovieDetails';
+import { ProgressBar } from 'react-loader-spinner';
 import css from './MovieDetails.module.css';
 import { useRef } from 'react';
 import IMAGE_DEFAULT_URL from '../images/default-poster.jpg';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const location = useLocation(); // для кнопки назад
   const backLinkLocationRef = useRef(location.state?.from ?? '/'); // для кноаки назад
@@ -18,6 +21,7 @@ const MovieDetails = () => {
       try {
         const movieData = await fetchMovieDetails(movieId);
         setMovie(movieData);
+        setIsLoading(false); // устанавливаем isLoading в false после получения данных
       } catch (error) {
         console.error(error);
       }
@@ -26,9 +30,9 @@ const MovieDetails = () => {
     getMovieDetails();
   }, [movieId]);
 
-  // пока идет запрос
+  //   пока идет запрос
   if (!movie) {
-    return <p>Loading...</p>;
+    return <InfinitySpin width="200" color="#4fa94d" />;
   }
 
   const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/w200/';
@@ -37,48 +41,68 @@ const MovieDetails = () => {
 
   return (
     <div className={css.container}>
-      <button className={css.button}>
-        {/*кнопка назад + Elvis */}
-        <Link to={backLinkLocationRef.current}>Go back</Link>
-      </button>
-      <div className={css.content}>
-        <img
-          src={
-            movie.poster_path
-              ? IMAGES_BASE_URL + movie.poster_path
-              : IMAGE_DEFAULT_URL
-          }
-          alt={movie.title}
-          className={css.image}
-        />
-        <div className={css.info}>
-          <h1>
-            {movie.original_title}({year})
-          </h1>
-          <p>User Score : {Math.round(movie.vote_average * 10)}%</p>
-
-          <h2>Overview</h2>
-          <p>{movie.overview}</p>
-          <h3>Genres</h3>
-          <ul>
-            {movie.genres.map(({ id, name }) => (
-              <li key={id}>{name}</li>
-            ))}
-          </ul>
+      {isLoading ? (
+        <div className="loader">
+          <InfinitySpin width="200" color="#4fa94d" />
         </div>
-      </div>
-      <div className={css.additional}>
-        <p>Aditional information</p>
-        <ul>
-          <li>
-            <Link to="cast">Cast</Link>
-          </li>
-          <li>
-            <Link to="reviews">Revievs</Link>
-          </li>
-        </ul>
-      </div>
-      <Suspense fallback={<div>Loading...</div>}>
+      ) : (
+        <div>
+          <button className={css.button}>
+            {/*кнопка назад + Elvis */}
+            <Link to={backLinkLocationRef.current}>Go back</Link>
+          </button>
+          <div className={css.content}>
+            <img
+              src={
+                movie.poster_path
+                  ? IMAGES_BASE_URL + movie.poster_path
+                  : IMAGE_DEFAULT_URL
+              }
+              alt={movie.title}
+              className={css.image}
+            />
+            <div className={css.info}>
+              <h1>
+                {movie.original_title}({year})
+              </h1>
+              <p>User Score : {Math.round(movie.vote_average * 10)}%</p>
+
+              <h2>Overview</h2>
+              <p>{movie.overview}</p>
+              <h3>Genres</h3>
+              <ul>
+                {movie.genres.map(({ id, name }) => (
+                  <li key={id}>{name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className={css.additional}>
+            <p>Aditional information</p>
+            <ul>
+              <li>
+                <Link to="cast">Cast</Link>
+              </li>
+              <li>
+                <Link to="reviews">Revievs</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+      <Suspense
+        fallback={
+          <ProgressBar
+            height="80"
+            width="80"
+            ariaLabel="progress-bar-loading"
+            wrapperStyle={{}}
+            wrapperClass="progress-bar-wrapper"
+            borderColor="#F4442E"
+            barColor="#51E5FF"
+          />
+        }
+      >
         <Outlet />
       </Suspense>
     </div>
