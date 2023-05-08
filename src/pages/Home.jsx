549eebcb3ea4eab164794ed3d9cbb550982df9cc
Denys <denys.drhn@gmail.com>
2023-05-08 23:45:33 +0300

    add InfinitySpin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import fetchTrendingMovies from '../services/fetchTrendingMovies';
 import MoviesList from 'components/MoviesList/MoviesList';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getMovies = async () => {
@@ -12,6 +14,8 @@ const Home = () => {
         setMovies(data.results);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getMovies();
@@ -20,7 +24,13 @@ const Home = () => {
   return (
     <div>
       <h1>Trending today</h1>
-      <MoviesList movies={movies} />
+      {isLoading ? (
+        <div className="loader">
+          <InfinitySpin width="200" color="#4fa94d" />
+        </div>
+      ) : (
+        <MoviesList movies={movies} />
+      )}
     </div>
   );
 };
